411bd8161c6740ba8ef755dcc1f84a8a6f2d8b2c
Riccardo <richi-97@live.it>
2025-12-02 15:46:31 +0100

    moved interfaces to external file and added loading spinner

--- src/assets/components/Home.tsx
+++ src/assets/components/Home.tsx
@@ -1,35 +1,11 @@
 import { useEffect, useState } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Spinner } from "react-bootstrap";
 const endpoint = "https://api.spaceflightnewsapi.net/v4/articles";
+import { type Articles } from "../types/interface";
 
 const Home = function () {
-  interface Articles {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: Result[];
-  }
-
-  interface Result {
-    id: number;
-    title: string;
-    authors: Author[];
-    url: string;
-    image_url: string;
-    news_site: string;
-    summary: string;
-    published_at: string;
-    updated_at: string;
-    featured: boolean;
-    launches: unknown[];
-    events: unknown[];
-  }
-
-  interface Author {
-    name: string;
-    socials: null;
-  }
   const [arrayOfArticles, setArrayOfArticles] = useState<Articles | null>(null);
+  const [loading, setLoading] = useState(true);
   const getArticles = function () {
     fetch(endpoint)
       .then((r) => {
@@ -42,9 +18,11 @@ const Home = function () {
       .then((dataArticles) => {
         console.log(dataArticles);
         setArrayOfArticles(dataArticles);
+        setLoading(false);
       })
       .catch((err) => {
         console.log("errore", err);
+        setLoading(false);
       });
   };
 
@@ -54,30 +32,36 @@ const Home = function () {
 
   return (
     <>
-      <title>Spaceflight News - Home</title>
-      <Container fluid>
-        <Row className="justify-content-center">
-          <Col xs={12} md={6}>
-            <h1 className="text-center">NEWS IN EVIDENZA</h1>
-            {arrayOfArticles.results.map((art) => {
-              return (
-                <Card key={art.id} className="mt-3">
-                  <Card.Img variant="top" src={art.image_url} />
-                  <Card.Body>
-                    <Card.Title>{art.title}</Card.Title>
-                    <Card.Text>{art.summary}</Card.Text>
-                    <Card.Text>
-                      Di: {art.authors[0].name} Pubblicato il :{" "}
-                      {art.published_at.slice(0, 10)}
-                    </Card.Text>
-                    <Button variant="primary">Leggi</Button>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-          </Col>
-        </Row>
-      </Container>
+      {loading ? (
+        <div className="text-center mt-3">
+          <Spinner animation="border" variant="dark"></Spinner>
+        </div>
+      ) : (
+        <Container fluid>
+          <title>Spaceflight News - Home</title>
+          <Row className="justify-content-center">
+            <Col xs={12} md={6}>
+              <h1 className="text-center">NEWS IN EVIDENZA</h1>
+              {arrayOfArticles.results.map((art) => {
+                return (
+                  <Card key={art.id} className="mt-3">
+                    <Card.Img variant="top" src={art.image_url} />
+                    <Card.Body>
+                      <Card.Title>{art.title}</Card.Title>
+                      <Card.Text>{art.summary}</Card.Text>
+                      <Card.Text>
+                        Di: {art.authors[0].name} Pubblicato il :{" "}
+                        {art.published_at.slice(0, 10)}
+                      </Card.Text>
+                      <Button variant="primary">Leggi</Button>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </Col>
+          </Row>
+        </Container>
+      )}
     </>
   );
 };
